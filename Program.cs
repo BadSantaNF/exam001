2a033ba7848202c0714d1085a204d79cf832f38a
BadSantaNF <zeronfmss@gmail.com>
2024-02-19 16:50:23 +0300

    создаем метот сортерующий символы

--- Program.cs
+++ Program.cs
@@ -3,9 +3,31 @@ string [] GetArrayStringConsole (string inCosolSimcol)
 {
     string[] arraySimvol = new string[inConsoleSimvol.Length];
     arraySimvol = inConsolSimvol.Split(",");
-    Return arraySimvol;
+    return arraySimvol;
 }
 
 // создаем метот сортерующий символы
 
-
+string[] ArrayTreeSimvol (string[] array)
+{
+    int count = 0
+    for ( int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <=3)
+        {
+            count++;
+        }
+    }
+    string[] ArrayTreeSimvol = new string[count]
+    for (int i = 0 ,j = 0 ; i < array.Length;i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            ArrayTreeSimvol[j] = simvol;
+            j++
+        }
+    }
+    return ArrayTreeSimvol;
+}
