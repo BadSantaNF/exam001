d6c59c71b410de5495be3f8dcdc401cad04c1bff
BadSantaNF <zeronfmss@gmail.com>
2024-02-19 16:40:18 +0300

    создаем метот для разделения строки

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// создаем метот для разделения строки
+string [] GetArrayStringConsole (string inCosolSimcol)
+{
+    string[] arraySimvol = new string[inConsoleSimvol.Length];
+    arraySimvol = inConsolSimvol.Split(",");
+    Return arraySimvol;
+}
+
+// создаем метот сортерующий символы
+
+
