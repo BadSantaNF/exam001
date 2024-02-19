9e04da943221bbfeb9fe2cdd400315d4934dad79
BadSantaNF <zeronfmss@gmail.com>
2024-02-19 17:29:15 +0300

    создаем код для вызова методов и обработки текста через консоль

--- Program.cs
+++ Program.cs
@@ -1,35 +1,36 @@
 ﻿// создаем метот для разделения строки
-string [] GetArrayStringConsole (string inCosolSimcol)
+string[] GetArrayStringConsole (string inConsolSimvol)
 {
-    string[] arraySimvol = new string[inConsoleSimvol.Length];
+    string[] arraySimvol = new string[inConsolSimvol.Length];
     arraySimvol = inConsolSimvol.Split(",");
     return arraySimvol;
 }
 
 // создаем метот сортерующий символы
 
-string[] ArrayTreeSimvol (string[] array)
+
+string[] GetArrayThreeSimvol (string[] array)
 {
-    int count = 0
-    for ( int i = 0; i < array.Length; i++)
-    {
-        string simvol = array[i];
-        if (simvol.Length <=3)
-        {
-            count++;
-        }
-    }
-    string[] ArrayTreeSimvol = new string[count]
-    for (int i = 0 ,j = 0 ; i < array.Length;i++)
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
     {
         string simvol = array[i];
         if (simvol.Length <= 3)
         {
-            ArrayTreeSimvol[j] = simvol;
-            j++
+            count++;
         }
     }
-    return ArrayTreeSimvol;
+    string[] arrayThreeSimvol = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+    }
+    return arrayThreeSimvol;
 }
 
 // создаем метот void print
@@ -38,8 +39,22 @@ void Print (string[]  array )
 {
     for (int i = 0 ; i < array.Length; i ++)
     {
-        Conole.Write($"{array[i]}, ");
+        Console.Write($"{array[i]}, ");
 
     }
     Console.WriteLine();
 }
+
+// Создаем код для вызова методов и обработки текста через консоль
+
+Console.Write("Введите набор символов через знак (,) - ");
+string inStringSimvol = Console.ReadLine();
+string[] arraySimvol = GetArrayStringConsole(inStringSimvol);
+Console.WriteLine();
+Console.Write("Введенные символы - ");
+Print(arraySimvol);
+Console.WriteLine();
+Console.Write("Введенные символы длинной меньше либо равны 3 - ");
+Print(GetArrayThreeSimvol(arraySimvol));
+Console.WriteLine();
+
